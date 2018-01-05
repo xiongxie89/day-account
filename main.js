d8935b4aad029462a39e353888c2fd164b7639c6
xiongxie89 <34756925+xiongxie89@users.noreply.github.com>
2018-01-05 08:42:04 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+requirejs.config({
+    baseUrl: './',
+    paths: {
+       
+        jquery:'/lib/jquery-3.2.1'
+    }
+});
 
+require(['framework', 'jquery'], function (f,jq) {
+    angular.bootstrap($("html"), [f.name]);
+});
